117422854dab09d1740217d2946393b10dacd0a3
Muhammad Fiqri T <smandakotser@gmail.com>
2022-04-19 19:26:18 +0700

    relate all model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
     validates :name, uniqueness: true
+    has_many :order_detail
+    has_many :category
 end
--- app/models/pesanan.rb
+++ app/models/pesanan.rb
@@ -1,2 +1,4 @@
 class Pesanan < ApplicationRecord
+    belongs_to :customer
+    has_one :order_detail
 end
