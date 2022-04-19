74e03d07d85a6fd371cb4c99236711642ad0938e
Muhammad Fiqri T <smandakotser@gmail.com>
2022-04-19 14:35:30 +0700

    destroy model order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,3 @@
 class Item < ApplicationRecord
+    validates :name, uniqueness: true
 end
--- spec/factories/orders.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :order do
-    order_date { "MyString" }
-    quantity { 1 }
-  end
-end
